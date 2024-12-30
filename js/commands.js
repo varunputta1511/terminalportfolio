27c697ac4c2af8e0c8781133cdba8372f5627b32
Varun Putta <78648795+varunputta1511@users.noreply.github.com>
2024-12-30 13:22:56 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,106 @@
+var dev = "#";
+var twitter = "https://twitter.com/thevarunputta";
+var linkedin = "https://www.linkedin.com/in/varunputta/";
+var instagram = "https://www.instagram.com/varunputta_/";
+var github = "https://github.com/varunputta1511";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "mailto:varunputta1511@gmail.com";
+var a = "theme0.html";
+var b = "theme1.html";
+var c = "#";
+var d = "#";
+
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Varun!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently an aspiring Data Scientist, and I am interning at Cognitive Generation Enterprises.</li>",
+  "<li>🌱 My fields of interest are:</li>",
+  `<pre class="whitespace-pre-wrap">
+        <span style="color :#9ccfd8;"> - Data Science</span>
+        <span style="color :#9ccfd8;"> - Machine Learning</span>
+        <span style="color :#9ccfd8;"> - AI and Analytics</span>
+        
+        </pre>`,
+  `<li><pre class="whitespace-pre-wrap">👯 I would love to collaborate with anyone interested in data-driven projects, machine learning models, or AI-driven solutions. I enjoy exploring new technologies and applying them to solve real-world problems. I am always eager to learn and improve my skills.</pre></li>`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my GitHub profile to view my projects and contributions.
+   *Use the GitHub command to take a look at my GitHub profile.</pre></li>`,
+  `<li>📫 Contact me at <a href="mailto:varunputta1511@gmail.com">varunputta1511@gmail.com</a></li>`,
+  "<br>"
+];
+
+
+social = [
+  "<br>",
+  
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">twitter.com/thevarunputta' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">linkedin/varunputta' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="blank">instagram/varunputta_' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">github/varunputta1511' +
+    "</a>",
+  
+  "<br>",
+];
+
+projects = [
+"heelo"
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">theme</span>       
+   <span class="command">↳</span>&nbsp;Explore different themes.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
+theme = [
+  "<br>",
+  
+  'a        <a href="' +
+    a +
+    '" >twitter.com/thevarunputta' +
+    "</a>",
+  'b      <a href="' +
+    b +
+    '" target="blank">linkedin/varunputta' +
+    "</a>",
+  'c      <a href="' +
+    c +
+    '" target="blank">instagram/varunputta_' +
+    "</a>",
+  'd         <a href="' +
+    d +
+    '" target="blank">github/varunputta1511' +
+    "</a>",
+  
+  "<br>",
+];
